4a8b71418bf89679618428e4bcd15c75d2db70bf
Joy <138622790+J0YSICK@users.noreply.github.com>
2026-04-10 11:37:31 -0400

    Maps & Directions

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,77 @@
 import 'package:flutter/material.dart';
-import 'userModel.dart';
-import 'constants.dart';
-import 'API_service.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MaterialApp(home:MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: RestAPI(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class RestAPI extends StatefulWidget {
-  const RestAPI({super.key});
+class _MyAppState extends State<MyApp> {
+  //create controller that controls the satellite images
+  late GoogleMapController mapController;
 
-  @override
-  State<RestAPI> createState() => _RestAPIState();
-}
+  static final LatLng _center = LatLng(45.5186468, -73.6679352); //latitude, longitude
+  final Set<Marker> _markers = {};
+  LatLng _currentMapLocation = _center;
 
-class _RestAPIState extends State<RestAPI> {
 
-  late List<UserModel>? _userModel = [];
 
-  @override
-  void initState() {
-    super.initState();
-    _getData();
-  }
 
-  void _getData() async {
-    _userModel = (await API_service().getUsers());
-    Future.delayed(Duration(seconds: 2)).then((value) => setState(() {}));
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(' REST API'),
-          centerTitle: true,
+      appBar: AppBar(
+        title: Text('Maps controller'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+              onMapCreated: _onMapCreated,
 
-        ),
-        backgroundColor: Colors.greenAccent,
-        body: _userModel == null || _userModel!.isEmpty ?
-        Center(
-          child: CircularProgressIndicator(),
-        ) : ListView.builder(
-            itemCount: _userModel!.length, itemBuilder: (context, index) {
-          return Card(
-              child:
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(_userModel![index].id.toString()),
-                      Text(_userModel![index].username),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(_userModel![index].email),
-                      Text(_userModel![index].website),
-                      Text(_userModel![index].address.city),
-                      Text(_userModel![index].address.geo.lat),
-                      Text(_userModel![index].address.geo.lng),
-                      Text(_userModel![index].company.catchPhrase),
-                    ],
-                  )
-                ],
-              )
-          );
-        }
-        )
+              initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 10.0),
+            markers: _markers,
+              onCameraMove: _onCameraMove,
+          ),
+          Padding(padding: EdgeInsets.all(14.0),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: FloatingActionButton(
+              onPressed: _onAddMarkerButtonPressed,
+              backgroundColor: Colors.blueAccent,
+              materialTapTargetSize: MaterialTapTargetSize.padded,
+              child: Icon(Icons.map, size: 30.0,),
+            ),
+          ),
+          )
+
+
+        ],
+      ),
     );
   }
-}
 
+  void _onMapCreated(GoogleMapController controller){
+    mapController = controller;
+  }
+
+  void _onCameraMove(CameraPosition position){
+    _currentMapLocation = position.target;
+  }
+
+  void _onAddMarkerButtonPressed(){
+    setState(() {
+      _markers.add(Marker(markerId: MarkerId(_currentMapLocation.toString()),
+      position: _currentMapLocation,
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta)));
+    });
+  }
+}
